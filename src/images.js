65a9ba2b6fd89ac1dc9b5688eb56a2861ea73c0e
Gabriel <contact.gabrielgil@gmail.com>
2019-05-27 10:44:41 +0200

    delete un usefull files and imports
    then regenerate a new build

--- src/images.js
+++
@@ -1,7 +0,0 @@
-import codeURL from "./code.png"
-
-const img = document.createElement("img")
-img.src = codeURL
-img.style = "background: #2B3A42; padding: 20px"
-img.width = 32
-document.body.appendChild(img)
