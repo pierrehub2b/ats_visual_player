930d7504b50eadcaa1405d8495a2ad871ef01c6d
Gabriel <contact.gabrielgil@gmail.com>
2019-06-12 09:44:58 +0200

    new versin of dist file

--- src/app.js
+++ src/app.js
@@ -9,15 +9,8 @@ var $ = require('jQuery');
 import { setupScreen } from './uploader';
 var upload = require('./uploader');
 export var input = $("#uploader");
-import fs from 'fs';
+const fs = require('fs');
 var directory = './ATSV';
-
-
-//$('head').append('<link rel="stylesheet" type="text/css" href="./webfonts/font.css">');
-
-export function filesTraitment(files) {
-  
-}
 
 input.on("change", function(event) {
   let listing = $("#listing");
@@ -42,12 +35,31 @@ input.on("change", function(event) {
   };
 });
 
-// fs.readdir(directory, (err, files) => {
-// 	if(err) {
-// 		// handle error; e.g., folder didn't exist
-// 	}
-//   // 'files' is an array of the files found in the directory
-//   filesTraitment();
+// fs.readdir(directory, function (err, files) {
+//   //handling error
+//   if (err) {
+//       return console.log('Unable to scan directory: ' + err);
+//   } 
+//   //listing all files using forEach
+//   files.forEach(function (file) {
+//       // Do whatever you want to do with the file
+//       for (let i=0; i<files.length; i++) {
+//         if(files[i].type == "application/ats.action-test-script.visual-report") {
+//           var fileName = files[i].name;
+//           // if(fileName.length > 35) {
+//           //   fileName = fileName.substring(0, 30) + "...";
+//           // }
+          
+//           var item = $("<li class='atsvList'><i class='fas fa-file-video'></i><p>"+ fileName +"</p></li>");
+//           item.on("click", function(e) {
+//             $(".atsvList > p").removeClass("bolder");
+//             e.target.classList.add("bolder");
+//             upload.openfile(files[i]);
+//           });
+//           listing.append(item);
+//         }
+//       };
+//   });
 // });
 
 setupScreen();
